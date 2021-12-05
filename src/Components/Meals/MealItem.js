32c0682ffe1e8cc13caa71d440c09ef7e3037946
mattvargas <vargassormm1@montclair.edu>
2021-12-05 12:53:29 -0500

    fixed id issues with input component so each component has a different id

--- src/Components/Meals/MealItem.js
+++ src/Components/Meals/MealItem.js
@@ -11,7 +11,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealForm />
+        <MealForm id={props.id} />
       </div>
     </li>
   );
