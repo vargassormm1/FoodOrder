763c00066bd2c671232c2a30fd9020f21c23b18e
mattvargas <vargassormm1@montclair.edu>
2021-12-07 00:48:41 -0500

    add to cart functionality

--- src/Components/Meals/MealForm.js
+++ src/Components/Meals/MealForm.js
@@ -1,10 +1,33 @@
+import { useRef, useState } from "react";
 import Input from "../UI/Input";
 import classes from "./MealForm.module.css";
 
 const MealForm = (props) => {
+  const amountInputRef = useRef();
+  const [amountValid, setAmountValid] = useState(true);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNumber);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: "amount_" + props.id,
@@ -16,6 +39,7 @@ const MealForm = (props) => {
         }}
       />
       <button>Add</button>
+      {!amountValid && <p>Please Enter A Valid Amount (1-5).</p>}
     </form>
   );
 };
--- src/Components/Meals/MealItem.js
+++ src/Components/Meals/MealItem.js
@@ -1,8 +1,20 @@
+import { useContext } from "react";
 import MealForm from "./MealForm";
 import classes from "./MealItem.module.css";
+import CartContext from "../../Store/CartContext";
 
 const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
   const price = `$${props.price.toFixed(2)}`;
+
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
   return (
     <li className={classes.meal}>
       <div>
@@ -11,7 +23,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealForm id={props.id} />
+        <MealForm id={props.id} onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
