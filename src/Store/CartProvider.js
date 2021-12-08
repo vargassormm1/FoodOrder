40ac41a722fb6a24290218c9fa0ae392283426a6
mattvargas <vargassormm1@montclair.edu>
2021-12-07 20:09:17 -0500

    fixed cartprovider function that list out cart items so it wont list out existing items

--- src/Store/CartProvider.js
+++ src/Store/CartProvider.js
@@ -8,12 +8,29 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
-    const updatedItems = state.items.concat(action.item);
-    const newTotalAmount =
+    const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
+
+    const existingCartIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    const existingCartItem = state.items[existingCartIndex];
+
+    let updatedItems;
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
     return {
       items: updatedItems,
-      totalAmount: newTotalAmount,
+      totalAmount: updatedTotalAmount,
     };
   }
   if (action.type === "REMOVE") {
